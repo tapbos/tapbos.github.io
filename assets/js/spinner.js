5ddbf3d06cb7182c3d71d4c2a9bf9d8d16e080fb
csylo <lo.christopher.355@gmail.com>
2020-10-24 15:00:37 -0400

    modal

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -133,14 +133,14 @@ function stopRotateWheel() {
     var index = Math.floor((360 - degrees % 360) / arcd);
     ctx.save();
     ctx.font = 'bold 30px Helvetica, Arial'; //centered results text
-    var choice = options[index];
-    var choiceInfo = optionsInfo[index];
-    ctx.fillText(choice, 250 - ctx.measureText(choice).width / 2, 250 + 10);
+    var text = options[index];
+    var textInfo = optionsInfo[index];
+    ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
     ctx.restore();
 
     modal.style.display = "block";
-    modalResult.innerHTML = "Let's eat at " + choice + "!";
-    modalInfo.innerHTML = choiceInfo;
+    modalResult.innerHTML = "Let's eat at " + text + "!";
+    modalInfo.innerHTML = textInfo;
 }
 
 span.onclick = function(){
