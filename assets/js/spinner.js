1d34af43f83c09d3c060f85ed9090749376b639c
clo355 <lo.christopher.355@gmail.com>
2021-01-09 05:11:38 -0500

    spinner text

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -73,7 +73,7 @@ function drawRouletteWheel() {
     if (canvas.getContext) {
 	var outsideRadius = 200;
 	var textRadius = 160;
-	var insideRadius = 55; /*125*/
+	var insideRadius = 55; /*55 - 125*/
 
 	ctx = canvas.getContext("2d");
 	ctx.clearRect(0,0,500,500);
@@ -160,14 +160,14 @@ function stopRotateWheel() {
     var index = Math.floor((360 - degrees % 360) / arcd);
     ctx.save();
     ctx.font = 'bold 30px Helvetica, Arial'; //centered results text
-    var text = ""; /*options index*/
+    var text = options[index]; /*options index*/
     var textInfo = optionsInfo[index]
     var textImage = optionsImage[index]
-    ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
+    //ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10); //text behind spin button
     ctx.restore();
 
     modal.style.display = "block";
-    console.log(optionsLinks[index]);
+    //console.log(optionsLinks[index]);
     modalResult.innerHTML = "Let's eat at <b><a href=" + optionsLinks[index] + " target=\"_blank\">" + text + "</a></b>!";
     //console.log("<img id=myModalImage src=" + optionsImage[index] + ">");
     modalImage.innerHTML = "<img id=myModalImage src=" + optionsImage[index] + ">";
